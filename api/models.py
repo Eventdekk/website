73cf3330b6373bc73d439ebe5a9f482f9907c040
Bumyy <57840717+Bumyy@users.noreply.github.com>
2023-11-10 08:29:37 +0100

    Add models

--- api/models.py
+++ api/models.py
@@ -5,3 +5,32 @@ class CounterModel(models.Model):
 
     def __str__(self):
         return "Counter"
+
+class UserModel(models.Model):
+    discord = models.CharField()
+
+class GroupModel(models.Model):
+    name = models.CharField()
+    owner_id = models.IntegerField()
+
+class GroupMembersModel(models.Model):
+    group_id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
+    role = models.SmallIntegerField()
+
+class EventModel(models.Model):
+    group_id = models.BigIntegerField()
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    ifc_link = models.URLField()
+    thumbnail = models.ImageField()
+
+class EventJoiningModel(models.Model):
+    event_id = models.BigIntegerField()
+    group_id = models.BigIntegerField()
+    departure = models.CharField(max_length=4)
+    arrival = models.CharField(max_length=4)
+    duration = models.DurationField()
+    est_pilots = models.SmallIntegerField()
+    act_pilots = models.SmallIntegerField() 
+
